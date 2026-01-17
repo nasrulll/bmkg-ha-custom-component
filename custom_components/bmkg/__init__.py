3083aeac03f3e8b03ec8af6df9ed119fa20240eb
nasrulll <59362113+nasrulll@users.noreply.github.com>
2026-01-17 08:54:22 +0800

    Update __init__.py

--- custom_components/bmkg/__init__.py
+++ custom_components/bmkg/__init__.py
@@ -1,1 +1,14 @@
 
+"""BMKG Custom Component for Home Assistant v1.7 FINAL + PRO+"""
+from homeassistant.helpers import discovery
+
+DOMAIN = "bmkg"
+
+async def async_setup(hass, config):
+    """Set up the BMKG component."""
+    # Placeholder, sensors & config_flow will handle actual setup
+    return True
+
+async def async_setup_entry(hass, entry):
+    """Set up from a config entry."""
+    return True
