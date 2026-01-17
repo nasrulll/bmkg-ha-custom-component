c21f0e6415237bfe181ad01f72179973a64ec26e
nasrulll <59362113+nasrulll@users.noreply.github.com>
2026-01-17 08:56:42 +0800

    Add BMKGConfigFlow for user configuration

--- custom_components/bmkg/config_flow.py
+++ custom_components/bmkg/config_flow.py
@@ -1,1 +1,17 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class BMKGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="BMKG Region", data=user_input)
+
+        data_schema = vol.Schema({
+            vol.Required("adm4"): str,
+            vol.Required("friendly_name", default="BMKG"): str,
+        })
+        return self.async_show_form(step_id="user", data_schema=data_schema)
 
